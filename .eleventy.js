59c7b1258dcc03dc2cf17c7ab51455f6cd8a9791
wolframkriesing <wolfram+github@kriesing.de>
2023-01-08 13:43:56 +0100

    Configure 11ty so the css dir and the index.html are deployed as they are, which means just simply take them online as is.
    And also configure "markdownTemplateEngine" so we can use nunjuck in the templates.

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  // Copy the css directory as is to the target site.
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('/index.html');
+  return {
+    passthroughFileCopy: true,
+    // Pre-process *.md files with nunjuck template engine.
+    markdownTemplateEngine: "njk",
+  }
+}
